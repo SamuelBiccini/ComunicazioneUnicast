e257687d723d31bc5c105d2914297000373ada05
Shibasu04 <xubuhan050418@gmail.com>
2025-02-18 13:35:02 +0100

    quarto commit: inizializzazione classe CLient

--- src/Client.java
+++ src/Client.java
@@ -5,6 +5,18 @@ import java.net.Socket;
 import java.net.SocketException;
 
 public class Client {
+    private String nome;
+    private String colore;
+    private Socket socket;
+public Client(String nomeDefault, String coloreDefault){
+    this.nome = nomeDefault;
+    this.colore = coloreDefault;
+}
+
+public void Conettore(String nomeServer, int portaServer ){
 
 
 }
+}
+
+
