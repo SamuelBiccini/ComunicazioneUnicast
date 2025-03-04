6754608425d2e995baa1a89f7d1cfc7984a71b7e
Filippo Mattioli <filippomattioli06@gmail.com>
2025-03-04 13:33:27 +0100

    modifica della classe client implementazione connettore

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,8 @@
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
@@ -8,15 +11,27 @@ public class Client {
     private String nome;
     private String colore;
     private Socket socket;
-public Client(String nomeDefault, String coloreDefault){
-    this.nome = nomeDefault;
-    this.colore = coloreDefault;
-}
+    private PrintWriter out;
+    private BufferedReader in;
+    private BufferedReader stdIn;
 
-public void Conettore(String nomeServer, int portaServer ){
+    public Client(String nomeDefault, String coloreDefault) {
+        this.nome = nomeDefault;
+        this.colore = coloreDefault;
+    }
 
+    public void conettore(String nomeServer, int portaServer) {
+        try {
+            socket = new Socket(nomeServer, portaServer);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            stdIn = new BufferedReader(new InputStreamReader(System.in));
 
-}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
 
 
